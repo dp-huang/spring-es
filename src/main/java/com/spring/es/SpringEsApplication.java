844f2eca9416866c22a2f0041a0c14fd6a868917
Huang <huangd7@cnenhuangd7m1.corp.emc.com>
2016-04-14 09:41:53 +0800

    init commit

---
+++ src/main/java/com/spring/es/SpringEsApplication.java
@@ -0,0 +1,12 @@
+package com.spring.es;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/spring/es/SpringEsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.es;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringEsApplication.class)
+public class SpringEsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
