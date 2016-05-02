76bcdee78b80118d003b1b422cf12826238a7187
dp-huang <yebigroc@gmail.com>
2016-05-02 22:20:15 +0800

    update dependency and add gitignore

--- src/main/java/com/spring/es/SpringEsApplication.java
+++ src/main/java/com/spring/es/SpringEsApplication.java
@@ -2,8 +2,12 @@ package com.spring.es;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.spring.es" })
 public class SpringEsApplication {
 
 	public static void main(String[] args) {
